446c3be3eab9346492903144cc638be6ca40bac7
Josh Winter <winterjoshr@gmail.com>
2018-11-28 22:09:29 -0500

    draw wormy text

--- src/main.cpp
+++ src/main.cpp
@@ -50,6 +50,10 @@ void render(Renderer &renderer)
 {
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		renderer.font.renderer.draw(renderer.font.main, "wormy", 0.0f, -4.0f, win::color(255, 255, 255), win::font_renderer::CENTERED);
+
+		glBindVertexArray(renderer.vao);
+		glUseProgram(renderer.program);
 		glBindTexture(GL_TEXTURE_2D, renderer.tpack[0]);
 		renderer.add(0.0f, 0.0f, 255, 255, 0);
 		renderer.send();

ef998b1e857b7275d79fc0c7d3f79b380dee924e
Josh Winter <winterjoshr@gmail.com>
2018-12-09 03:50:33 -0500

    change collision algorithm

--- src/entity/Entity.cpp
+++ src/entity/Entity.cpp
@@ -1,6 +1,14 @@
+#include <cmath>
+
 #include <wormy.h>
 
 bool Entity::collide(const Entity &other) const
 {
-	return x + s > other.x && x < other.x + other.s && y + s > other.y && y < other.y + other.s;
+	const float center_x1 = x + (s / 2.0f);
+	const float center_y1 = y + (s / 2.0f);
+
+	const float center_x2 = other.x + (other.s / 2.0f);
+	const float center_y2 = other.y + (other.s / 2.0f);
+
+	return std::sqrtf(std::powf(center_x1 - center_x2, 2) + std::powf(center_y1 - center_y2, 2)) < (s / 2.0f) + (other.s / 2.0f);
 }
